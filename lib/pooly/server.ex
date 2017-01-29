50be373772be0a2e30f69cb5328fe209c57c9ea4
Tomoaki Tsuruta <england0112@gmail.com>
2017-01-29 09:07:14 -0800

    Create version4

--- lib/pooly/server.ex
+++ lib/pooly/server.ex
@@ -2,100 +2,59 @@ defmodule Pooly.Server do
   use GenServer
   import Supervisor.Spec
 
-  defmodule State do
-    defstruct sup: nil, size: nil, worker_sup: nil, workers: nil, mfa: nil, monitors: nil
+  #######
+  # API #
+  #######
+
+  def start_link(pools_config) do
+    GenServer.start_link(__MODULE__, pools_config, name: __MODULE__)
   end
 
-  def start_link(sup, pool_config) do
-    GenServer.start_link(__MODULE__, [sup, pool_config], name: __MODULE__)
+  def checkout(pool_name, block, timeout) do
+    Pooly.PoolServer.checkout(pool_name, block, timeout)
   end
 
-  def checkin(worker_pid) do
-    GenServer.cast(__MODULE__, {:check_in, worker_pid})
+  def checkin(pool_name, worker_pid) do
+    Pooly.PoolServer.checkin(pool_name, worker_pid)
   end
 
-  def checkout do
-     GenServer.call(__MODULE__, :checkout)
-  end
-
-  def status do
-    GenServer.call(__MODULE__, :status)
-  end
-
-  def init([sup, pool_config]) when is_pid(sup) do
-    monitors = :ets.new(:monitors, [:private])
-    init(pool_config, %State{sup: sup, monitors: monitors})
-  end
-
-  def init([{:mfa, mfa}|rest], state) do
-    init(rest, %{state | mfa: mfa})
-  end
-
-  def init([{:size, size}|rest], state) do
-    init(rest, %{state | size: size})
-  end
-
-  def init([_|rest], state) do
-    init(rest, state)
-  end
-
-  def init([], state) do
-    send(self, :start_worker_supervisor)
-    {:ok, state}
-  end
-
-  def handle_call(:status, _from, %{workers: workers, monitors: monitors} = state) do
-    {:reply, {length(workers), :ets.info(monitors, :size)}, state}
-  end
-
-  def handle_call(:checkout, {from_pid, _ref}, %{workers: workers, monitors: monitors} = state) do
-    case workers do
-      [worker|rest] ->
-        ref = Process.monitor(from_pid)
-        true = :ets.insert(monitors, {worker, ref})
-        {:reply, worker, %{state | workers: rest}}
-      [] ->
-        {:reply, :noproc, state}
+  def transaction(pool_name, fun, timeout) do
+    worker = checkout(pool_name, true, timeout)
+    try do
+      fun.(worker)
+    after
+      checkin(pool_name, worker)
     end
   end
 
-  def handle_cast({:check_in, worker}, %{workers: workers, monitors: monitors} = state) do
-    case :ets.lookup(monitors, worker) do
-      [{pid, ref}] ->
-        true = Process.demonitor(ref)
-        true = :ets.delete(monitors, pid)
-        {:noreply, %{state | workers: [pid|workers]}}
-      [] ->
-        {:noreply, state}
-    end
+  def status(pool_name) do
+    Pooly.PoolServer.status(pool_name)
   end
 
-  def handle_info(:start_worker_supervisor, state = %{sup: sup, mfa: mfa, size: size}) do
-    {:ok, worker_sup} = Supervisor.start_child(sup, supervisor_spec(mfa))
-    workers = prepopulate(size, worker_sup)
-    {:noreply, %{state | worker_sup: worker_sup, workers: workers}}
+  #############
+  # Callbacks #
+  #############
+
+  def init(pools_config) do
+    pools_config |> Enum.each(fn(pool_config) ->
+      send(self, {:start_pool, pool_config})
+    end)
+
+    {:ok, pools_config}
   end
 
-  defp supervisor_spec(mfa) do
-    opts = [restart: :temporary]
-    supervisor(Pooly.WorkerSupervisor, [mfa], opts)
+  def handle_info({:start_pool, pool_config}, state) do
+    {:ok, _pool_sup} = Supervisor.start_child(Pooly.PoolsSupervisor, supervisor_spec(pool_config))
+    {:noreply, state}
   end
 
-  defp prepopulate(size, sup) do
-    prepopulate(size, sup, [])
-  end
+  #####################
+  # Private Functions #
+  #####################
 
-  defp prepopulate(size, _sup, workers) when size < 1 do
-    workers
-  end
-
-  defp prepopulate(size, sup, workers) do
-    prepopulate(size-1, sup, [new_worker(sup) | workers])
-  end
-
-  defp new_worker(sup) do
-    {:ok, worker} = Supervisor.start_child(sup, [[]])
-    worker
+  defp supervisor_spec(pool_config) do
+    opts = [id: :"#{pool_config[:name]}Supervisor"]
+    supervisor(Pooly.PoolSupervisor, [pool_config], opts)
   end
 
 end
